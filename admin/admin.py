a18346f5c330c184f8da664f2ec07ffb5fa5592e
smakaren <s.makarenka@stud.elka.pw.edu.pl>
2023-06-20 09:44:16 +0200

    login feature

--- admin/admin.py
+++ admin/admin.py
@@ -2,4 +2,9 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from database import get_session, metadata
 
 admin_bp = Blueprint('admin_bp', __name__,
-                    static_folder='static')
+                    template_folder='templates/admin',  static_folder='static')
+
+
+@admin_bp.route('/')
+def admin_dashboard():
+    return render_template('admin_main.html')
