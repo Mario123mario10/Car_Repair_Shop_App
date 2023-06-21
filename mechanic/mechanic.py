0e602b3e32143cd00b5b77d79b0c32e3578c147c
pgrabow1 <01149927@pw.edu.pl>
2023-06-22 00:09:56 +0200

    Added Task and part in magazines features

--- mechanic/mechanic.py
+++ mechanic/mechanic.py
@@ -9,6 +9,7 @@ mechanic_bp = Blueprint('mechanic_bp', __name__,
 # Define your SQL queries
 sql_queries = {
     'table1': "SELECT marka AS \"Marka pojazdu\", model AS \"Model Pojazdu\", rok_produkcji AS \"Rok produkcji pojazdu\" FROM pojazd",
+    'zlecenie': "SELECT id_zlec AS \"Numer Zlecenia\", opis_przed_naprawa AS \"Opis przed naprawą\", data_przyjecia AS \"Data przyjęcia\", szacowany_czas_naprawy AS \"Szacowany czas naprawy\", pojazd_id_poj AS \"ID Pojazdu\", status_id_stat AS \"ID Statusu\", mechanik_id_uż AS \"ID Mechanika\" FROM zlecenie WHERE mechanik_id_uż = 4",
     'table2': "SELECT id_masz AS \"Numer maszyny\", koszt AS \"Koszt maszyny\", data_zakupu AS \"data zakupu\", opis_stanu as \"stan maszyny\" FROM maszyna",
     'table3': "SELECT id_mag \"Id magazynu\", nazwa AS \"Nazwa magazynu\", pojemnosc_m3 AS \"Pojemnosc\" FROM magazyn",
     'reservation': "SELECT r.nazwa AS \"Nazwa części\", SUM(p.liczba_sztuk) AS \"Liczba sztuk\" FROM pozmag p JOIN rodzajczesci r ON p.rodzajczesci_id_czesci = r.id_czesci WHERE p.magazyn_id_mag = :condition_value GROUP BY r.nazwa"
@@ -51,3 +52,15 @@ def reserve(message):
     session.close()
     #flash('Reservation successful!', 'success')
     return render_template('mechanic_main.html', table_id='reservation', data=data)
+
+@mechanic_bp.route('/zlecenie/<message>')
+def zlecenie(message):
+    session = get_session()
+    sql_query = text(sql_queries.get('zlecenie'))
+    print(message)
+    message = 4
+    result_proxy = session.execute(sql_query, {'condition_value': message})
+    keys = result_proxy.keys()
+    data = [dict(zip(keys, row)) for row in result_proxy]
+    session.close()
+    return render_template('mechanic_main.html', table_id='zlecenie', data=data)
