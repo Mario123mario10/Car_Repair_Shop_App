44ea35b48c039da1508748c6ddc8a691a83b93b1
pgrabow1 <01149927@pw.edu.pl>
2023-06-21 21:19:24 +0200

    added feature forthings in magazine

--- mechanic/mechanic.py
+++ mechanic/mechanic.py
@@ -10,7 +10,9 @@ mechanic_bp = Blueprint('mechanic_bp', __name__,
 sql_queries = {
     'table1': "SELECT marka AS \"Marka pojazdu\", model AS \"Model Pojazdu\", rok_produkcji AS \"Rok produkcji pojazdu\" FROM pojazd",
     'table2': "SELECT id_masz AS \"Numer maszyny\", koszt AS \"Koszt maszyny\", data_zakupu AS \"data zakupu\", opis_stanu as \"stan maszyny\" FROM maszyna",
-    'table3': "SELECT marka AS \"Marka pojazdu\", model AS \"Model Pojazdu\", rok_produkcji AS \"Rok produkcji pojazdu\" FROM pojazd",
+    'table3': "SELECT id_mag \"Id magazynu\", nazwa AS \"Nazwa magazynu\", pojemnosc_m3 AS \"Pojemnosc\" FROM magazyn",
+    'reservation': "SELECT r.nazwa AS \"Nazwa części\", SUM(p.liczba_sztuk) AS \"Liczba sztuk\" FROM pozmag p JOIN rodzajczesci r ON p.rodzajczesci_id_czesci = r.id_czesci WHERE p.magazyn_id_mag = :condition_value GROUP BY r.nazwa"
+
 }
 
 @mechanic_bp.route('/')
@@ -37,3 +39,15 @@ def print_message(message):
     flash('Book complited!', 'success')
     print(message)
     return redirect(url_for('mechanic_bp.mechanic_dashboard'))
+
+@mechanic_bp.route('/reserve/<message>')
+def reserve(message):
+    session = get_session()
+    sql_query = text(sql_queries.get('reservation'))
+    print(message)
+    result_proxy = session.execute(sql_query, {'condition_value': message})
+    keys = result_proxy.keys()
+    data = [dict(zip(keys, row)) for row in result_proxy]
+    session.close()
+    #flash('Reservation successful!', 'success')
+    return render_template('mechanic_main.html', table_id='reservation', data=data)
